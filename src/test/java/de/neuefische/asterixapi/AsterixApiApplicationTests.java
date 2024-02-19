09694a86e7d4e4c423946168be3fa3695b4eb38c
Dominic <andro5@web.de>
2024-02-19 14:58:57 +0100

    Initial commit

---
+++ src/test/java/de/neuefische/asterixapi/AsterixApiApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.asterixapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsterixApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
